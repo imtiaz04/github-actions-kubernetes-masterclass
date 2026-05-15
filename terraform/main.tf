f56fa0db5db3cbbb816e544fd187094896602ea6
Imtiaz <Imtiaz.4.us@gmail.com>
2026-05-15 17:33:41 -0400

    fix: pin terraform provider versions

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.31.0"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 4.0"
+      version = "~> 4.0.4"
     }
   }
 }
